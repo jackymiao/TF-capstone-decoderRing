cdc4aafe324075023c788550e418c9e5d270c738
jackymiao <55778701+jackymiao@users.noreply.github.com>
2023-06-28 10:35:11 -0500

    add comment to sustitution file

--- src/substitution.js
+++ src/substitution.js
@@ -12,25 +12,29 @@ const substitutionModule = (function () {
     's', 't', 'u', 'v', 'w', 'x',
     'y', 'z'
   ];
-
+//input (a string), alphabet (a string with a default value of an empty string), and encode (a boolean with a default value of true). It performs encoding or decoding operations using the substitution cipher algorithm and a given substitution alphabet.
   function substitution(input, alphabet='', encode = true) {
-    // your solution code here
-    //get the array of inputAlphabet
-    //for input word, get indexOf standAlph, then use index to inputAlphabet, get the letter
-    //to decode, get indexOf subAlphabet, 
-    //special letters,don't skip, check subAlphabet.length, not 26, return false,
-    //check subAlphabet duplicated letter, true, return false.
+    //The code defines a helper function called distinct. This function is used to filter out duplicate values from an array. It returns true for the first occurrence of each distinct value in the array and false for subsequent occurrences.
     const distinct = (value, index, self) =>{
       return self.indexOf(value) === index
     };
-    //check unique letter and length
     const subAlphabet = alphabet.split("");
+    //The code checks if the provided alphabet meets the requirements for a valid substitution alphabet:
+    //The length of subAlphabet is not equal to the length of subAlphabet filtered with the distinct function. This ensures that all elements in subAlphabet are distinct.
+    //The length of subAlphabet is equal to 26, indicating that it contains all 26 letters of the English alphabet.
+    //The alphabet string is not empty ('').
+    //If any of these conditions is not met, the function returns false to indicate an invalid substitution alphabet.
     if(subAlphabet.length !== subAlphabet.filter(distinct).length||subAlphabet.length !== 26|| alphabet === ''){
       return false;
     };
-    //encode
+    // If encode is true, the function performs encoding.
     if(encode === true){
+      //The input string is converted to lowercase and split into an array called inputArr, where each element represents a single character.
       const inputArr = input.toLowerCase().split("");
+      // This array is created to store the encoded message.
+      // The map function is used to iterate over each element in inputArr. 
+      //If the element is not a space, its index in the standAlph array is obtained using indexOf. The corresponding letter from the subAlphabet array is added to transferArr. 
+      //If the element is a space, it is added as is to transferArr.
       const transferArr = inputArr.map((element)=>{
         if(element !== " "){
           const num = standAlph.indexOf(element);
@@ -39,11 +43,14 @@ const substitutionModule = (function () {
           return " "
         }
       })
+      // transferArr is joined into a string and returned as the encoded message.
       return transferArr.join("");
     }else{
+      //If encode is false, the function performs decoding.
       const inputArr = input.split("");
       const transferArr = inputArr.map((element)=>{
         if(element !== " "){
+          //The code is similar to the encoding logic, but in this case, the index of the element in the subAlphabet array
           const num = subAlphabet.indexOf(element);
           return standAlph[num]
         }else{
@@ -52,12 +59,6 @@ const substitutionModule = (function () {
       })
       return transferArr.join("");
     }
-
-
-
-
-
-
   }
 
   return {
